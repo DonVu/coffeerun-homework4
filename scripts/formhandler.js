ba29c02df0e33c73dc4048003d18f6eb1fa04dde
Don Vu <don.vu1212@gmail.com>
2018-03-05 19:47:01 -0800

    Create formhandler.js stub

---
+++ scripts/formhandler.js
@@ -0,0 +1,12 @@
+(function (window) {
+  "use strict";
+  var App = window.App || {};
+
+  function FormHandler() {
+    // Code goes here
+  }
+
+  App.FormHandler = FormHandler;
+  window.App = App;
+
+})(window);
